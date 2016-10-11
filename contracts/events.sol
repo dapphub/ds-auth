6a9e07773bffe8ab4a590ba7c01f1df2875d0e97
Andy Milenius <andrew.milenius@gmail.com>
2016-10-11 16:29:42 -0700

    adding pragmas

--- contracts/events.sol
+++ contracts/events.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.2;
+
 import 'enum.sol';
 
 contract DSAuthorizedEvents is DSAuthModesEnum {
--- contracts/util.sol
+++ contracts/util.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.2;
+
 import 'authorized.sol';
 import 'enum.sol';
 
