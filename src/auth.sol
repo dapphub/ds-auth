2d9ff16a440c802b9e6e800af1bee387db214d5e
Nikolai Mushegian <nikolai@nexusdev.us>
2017-02-02 12:59:36 -0500

    licenses

--- src/auth.sol
+++ src/auth.sol
@@ -1,5 +1,5 @@
 /*
-   Copyright 2017 Nexus Development, LLC
+   Copyright 2016-2017 Nexus Development, LLC
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
